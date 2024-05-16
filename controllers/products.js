572d5b022a0ae5747fce7aa6d8a56239b463c1f5
success96 <lfawas6@gmail.com>
2024-05-16 16:33:09 +0100

    added product validation

--- controllers/products.js
+++ controllers/products.js
@@ -6,7 +6,7 @@ const getAllProductsStatic = async (req, res) =>{
 }
 
 const getAllProducts = async (req, res) =>{
-    const {featured, company, name} = req.query;
+    const {featured, company, name, sort, fields} = req.query;
 
     const queryObject = {};
 
@@ -17,9 +17,29 @@ const getAllProducts = async (req, res) =>{
         queryObject.company = company
     }
     if(name){
-        queryObject.name = name
+        queryObject.name = {$regex: name, $options: 'i'}
     }
-    const products = await Product.find(queryObject)
+    let result = Product.find(queryObject)
+
+    if(sort){
+        const sortList = sort.split(',').join(' ');
+        result = result.sort(sortList);
+    } else {
+        result = result.sort('createdAt')
+    }
+
+    if(fields){
+        const fieldsList = fields.split(',').join(' ');
+        result = result.select(fieldsList);
+    }
+
+    const page = Number(req.query.page) || 1;
+    const limit = Number(req.query.limit) || 10;  
+    const skip = (page - 1) * limit;
+
+    result = result.skip(skip).limit(limit)
+
+    const products = await result;
     res.status(200).json({msg: products, nbHits: products.length})
 
 }
