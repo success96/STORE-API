eb3b6ffd201620f984f85c780792492b8d3fdd2c
success96 <lfawas6@gmail.com>
2024-05-29 04:30:00 +0100

    get all products endpoint completed

--- controllers/products.js
+++ controllers/products.js
@@ -6,7 +6,7 @@ const getAllProductsStatic = async (req, res) =>{
 }
 
 const getAllProducts = async (req, res) =>{
-    const {featured, company, name, sort, fields} = req.query;
+    const {featured, company, name, sort, fields, numericFilters} = req.query;
 
     const queryObject = {};
 
@@ -19,6 +19,35 @@ const getAllProducts = async (req, res) =>{
     if(name){
         queryObject.name = {$regex: name, $options: 'i'}
     }
+    if(numericFilters){
+        const operatorMap = {
+            '>': '$gt',
+            '>=': '$gte',
+            '=': '$eq',
+            '<': '$lt',
+            '<=': '$lte'
+        }
+        const regEx = /\b(<|>|>=|=|<=)\b/g;
+        //console.log(numericFilters);
+        let filters = numericFilters.replace(
+            regEx, 
+            (match)=>`-${operatorMap[match]}-`
+        );
+        console.log(filters);
+        const options = ['price', 'rating'];
+        filters = filters.split(',').forEach((item)=>{
+            const [field, operator, value] = item.split('-');
+            if (options.includes(field)){
+                queryObject[field] = {[operator]: Number(value)}
+            }
+        })
+
+    };
+
+    console.log(queryObject)
+
+
+
     let result = Product.find(queryObject)
 
     if(sort){
